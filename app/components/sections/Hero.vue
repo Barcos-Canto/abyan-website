23d2a5b78f8099222d7a60077136377e0834232b
cesar <cesarcantopaul@gmail.com>
2026-07-15 11:33:49 +0800

    added initial content

--- app/components/sections/Hero.vue
+++ app/components/sections/Hero.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>
-        <section id="hero">
-            <h2>Hero-Landing</h2>
+    <UContainer
+        class="bg-abyan-blue-500"
+    >
+        <section id="hero" class="flex flex-col md:flex-row items-center gap-8 px-6 md:px-16 py-12 text-abyan-white">
+            <!-- LEFT -->
+            <div class="flex flex-col gap-2 md:w-3/5">
+                <h1>
+                    Building Relationships Between Labor And Capital
+                </h1>
+                <p>
+                    Uniting workforce and management for mutual growth and success. 
+                    From market beginnings to industry impact. Abyan bridges gaps 
+                    between workforce and management, building strong foundations for 
+                    career growth and mutual success.
+                </p>
+            </div>
+
+            <!-- RIGHT -->
+            <img
+                src="/logos/abyan-logo-white-cropped.png"
+                class="w-full md:w-2/5"
+            ></img>
         </section>
-    </div>
+    </UContainer>
 </template>
