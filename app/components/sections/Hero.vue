b3ae6e176e7fcbd9f29c34ae6a73e418ef0939e9
cesar <cesarcantopaul@gmail.com>
2026-07-15 14:54:47 +0800

    modified section responsiveness

--- app/components/sections/Hero.vue
+++ app/components/sections/Hero.vue
@@ -1,26 +1,28 @@
 <template>
-    <UContainer
-        class="bg-abyan-blue-500"
-    >
-        <section id="hero" class="flex flex-col md:flex-row items-center gap-8 px-6 md:px-16 py-12 text-abyan-white">
-            <!-- LEFT -->
-            <div class="flex flex-col gap-2 md:w-3/5">
-                <h1>
-                    Building Relationships Between Labor And Capital
-                </h1>
-                <p>
-                    Uniting workforce and management for mutual growth and success. 
-                    From market beginnings to industry impact. Abyan bridges gaps 
-                    between workforce and management, building strong foundations for 
-                    career growth and mutual success.
-                </p>
-            </div>
-
-            <!-- RIGHT -->
-            <img
-                src="/logos/abyan-logo-white-cropped.png"
-                class="w-full md:w-2/5"
-            ></img>
-        </section>
-    </UContainer>
+    <div class="w-full bg-abyan-blue-500" >
+        <UContainer>
+            <section id="hero" class="flex flex-col md:flex-row items-center px-6 gap-16 md:px-16 py-12 text-abyan-white">
+                <div class="flex flex-col gap-4 md:mt-8 md:w-3/5">
+                    <h1>
+                        Building Relationships Between Labor And Capital
+                    </h1>
+                    <p>
+                        Uniting workforce and management for mutual growth and success. 
+                        From market beginnings to industry impact. Abyan bridges gaps 
+                        between workforce and management, building strong foundations for 
+                        career growth and mutual success.
+                    </p>
+                </div>
+    
+                <!-- RIGHT -->
+                <div class="w-full md:mt-8 md:w-2/5">
+                    <img
+                        src="/logos/abyan-logo-white-cropped.png"
+                        class="w-full h-auto object-contain"
+                        alt="Abyan Logo"
+                    ></img>
+                </div>
+            </section>
+        </UContainer>
+    </div>
 </template>
