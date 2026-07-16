97a1a7f83cf3e37fae4a0413d5a1deff4a355fa6
cesar <cesarcantopaul@gmail.com>
2026-07-16 10:09:04 +0800

    fixed typo

--- app/components/sections/ContactUs.vue
+++ app/components/sections/ContactUs.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <section>
-            <h2>Contact Us</h2>
-        </section>
+    <div id="contact-us" class="w-full px-6 md:px-16 py-12 bg-abyan-blue-500" >
+        <UContainer>
+            <section>
+                <div>
+                    <h2>Contact Us</h2>
+                </div>
+            </section>
+        </UContainer>
     </div>
 </template>
