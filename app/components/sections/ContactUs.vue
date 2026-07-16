c22656511ad2e47ee058d076644916a5180cc80f
cesar <cesarcantopaul@gmail.com>
2026-07-16 14:42:49 +0800

    contact us --draft 1

--- app/components/sections/ContactUs.vue
+++ app/components/sections/ContactUs.vue
@@ -1,10 +1,39 @@
 <template>
-    <div id="contact-us" class="w-full px-6 md:px-16 py-12 bg-abyan-blue-500" >
+    <div id="contact-us" class="w-full px-6 md:p-16 py-4 bg-abyan-blue-700">
         <UContainer>
-            <section>
-                <div>
-                    <h2>Contact Us</h2>
+            <section class="grid items-start gap-8 md:grid-cols-3">
+
+                <div class="gap-4 text-abyan-white">
+                    <h2>Get In Toooouch</h2>
+                    <p class="text-sm">
+                        Bridging Relationships Between Labor and Capital. 
+                        Reach out today to discuss a business partnership, or submit your 
+                        resume to join our talent pool. You can contact our team directly 
+                        or visit our main office.
+                    </p>
                 </div>
+
+                <div class="grid gap-4 md:grid-cols-2 md:col-span-2">                    
+                    <div class="rounded-2xl p-6 bg-abyan-blue-100">
+                        <p class="py-2 text-abyan-blue-500">Call us</p>
+                        <p class="text-xs font-bold text-abyan-blue-800">(02)8731 6384</p>
+                    </div>
+                
+                    <div class="rounded-2xl p-6 bg-abyan-blue-100">
+                        <p class="py-2 text-abyan-blue-500">Send us your resume</p>
+                        <p class="text-xs font-bold text-abyan-blue-800">yourfuturestartshere2025@gmail.com</p>
+                    </div>
+
+                    <div class="rounded-2xl p-6 bg-abyan-blue-100"> 
+                        <p class="py-2 text-abyan-blue-500">Visit us</p>
+                        <p class="text-xs font-bold text-abyan-blue-800">3rd Floor, Unit 18, Legazpi Tower, 2600 Roxas Blvd, Malate, Manila, 1004 Metro Manila</p>
+                        <p class="mt-4 text-xs font-bold text-shadow-abyan-blue-800">Our offices are open during business hours</p>
+                    </div>
+
+                    <div class="hidden rounded-2xl p-6 bg-abyan-white">
+                    </div>
+                </div>
+
             </section>
         </UContainer>
     </div>
