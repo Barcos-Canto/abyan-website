6801cfa3af0bf3d6cf776b3534d002ce22e5900f
cesar <cesarcantopaul@gmail.com>
2026-07-16 09:47:12 +0800

    adjusted margins and structure

--- app/components/sections/Hero.vue
+++ app/components/sections/Hero.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-full bg-abyan-blue-500" >
+    <div class="w-full px-6 md:px-16 py-12 bg-abyan-blue-500" >
         <UContainer>
-            <section id="hero" class="flex flex-col md:flex-row items-center px-6 gap-16 md:px-16 py-12 text-abyan-white">
-                <div class="flex flex-col gap-4 md:mt-8 md:w-3/5">
+            <section id="hero" class="flex flex-col md:flex-row items-center gap-16 mt-10 text-abyan-white">
+                <div class="flex flex-col gap-4 md:w-3/5">
                     <h1>
                         Building Relationships Between Labor And Capital
                     </h1>
@@ -15,7 +15,7 @@
                 </div>
     
                 <!-- RIGHT -->
-                <div class="w-full md:mt-8 md:w-2/5">
+                <div class="w-full md:w-2/5">
                     <img
                         src="/logos/abyan-logo-white-cropped.png"
                         class="w-full h-auto object-contain"
