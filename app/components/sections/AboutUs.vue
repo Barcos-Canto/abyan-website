0ccf48d1af7c57550eb8c101ea935299b08fbf33
cesar <cesarcantopaul@gmail.com>
2026-07-16 16:06:10 +0800

    added contents

--- app/components/sections/AboutUs.vue
+++ app/components/sections/AboutUs.vue
@@ -1,7 +1,31 @@
 <template>
-    <div>
-        <section>
-            <h2>About Us</h2>
-        </section>
+    <div id="partners" class="w-full px-6 md:px-16 py-12 bg-abyan-white">
+        <UContainer>
+            <section>
+                <div>
+                    <div class="text-center">
+                        <h2>About Us</h2>
+                        <p>
+                            Founded in November 2015, Abyan Mgt. Services Corp. set out to bridge the relationship 
+                            between labor and capital. We're a corporation duly registered with the Securities and 
+                            Exchange Commission, and a legitimate service contractor under DOLE Department Order No. 174.
+                        </p>
+                        <p>
+                            As we grow, innovation in how we work will be continuous — but the dignity of work and the 
+                            rights of our workers will always come first.
+                        </p>
+                    </div>
+                    <UMarquee>
+                        <div>
+                            <img src="/images/man-digging-soil.jpg" class="w-30 rounded-3xl overflow-hidden">
+                            <img src="/images/man-operating-farm-equipment.jpg" class="w-30 rounded-3xl overflow-hidden">
+                            <img src="/images/man-spraying-plants.jpg" class="w-30 rounded-3xl overflow-hidden">
+                            <img src="/images/men-group-photo-v1.jpg" class="w-30 rounded-3xl overflow-hidden">
+                            <img src="/images/mwn-sorting-fruit.jpg" class="w-30 rounded-3xl overflow-hidden">
+                        </div>
+                    </UMarquee>
+                </div>
+            </section>
+        </UContainer>
     </div>
 </template>
