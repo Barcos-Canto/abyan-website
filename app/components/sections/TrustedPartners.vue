99c6d982f6c92c256a8ed32233336dd5e8f5f500
cesar <cesarcantopaul@gmail.com>
2026-07-16 10:53:48 +0800

    fixed structure

--- app/components/sections/TrustedPartners.vue
+++ app/components/sections/TrustedPartners.vue
@@ -2,32 +2,30 @@
     <div id="partners" class="w-full px-6 md:px-16 py-12 bg-abyan-white">
         <UContainer>
             <section>
-                <div>
-                    <div class="text-center">
-                        <h6>Fostering partnerships since 2015</h6>
-                        <h1>Trusted By Known Brands</h1>
-                    </div>
+                <div class="text-center">
+                    <h6>Fostering partnerships since 2015</h6>
+                    <h1>Trusted By Known Brands</h1>
+                </div>
 
-                    <!-- BRANDS -->
-                    <div class="rounded-b-lg shadow-base-300/20 shadow-lg mx-auto mt-10 grid max-w-xl items-center gap-x-8 gap-y-10 pb-8 grid-cols-2 md:grid-cols-5 md:mx-0 md:max-w-none">
-                        <img width="158" height="48" src="/brands/dominos-pizza.webp" alt="Dominos Pizza Logo" class="max-h-12 w-full object-contain"/>
-                        <img width="158" height="48" src="/brands/mcdo.webp" alt="Mcdo Logo" class=" max-h-12 w-full object-contain"/>
-                        <img width="158" height="48" src="/brands/san-miguel.webp" alt="San Miguel Corporation Logo" class="max-h-12 w-full object-contain"/>
-                        <img width="158" height="48" src="/brands/anchor-land.png" alt="Anchor Land Holdings Logo" class=" max-h-12 w-full object-cover"/>
-                        <img width="158" height="48" src="/brands/megaworld.png" alt="Megaworld Logo" class=" max-h-12 w-full object-contain"/>
-                        
-                        <img width="158" height="48" src="/brands/honda.png" alt="Honda Logo" class=" max-h-12 w-full object-contain" />
-                        <img width="158" height="48" src="/brands/hyundai.png" alt="Hyundai Logo" class=" max-h-12 w-full object-contain" />
-                        <img width="158" height="48" src="/brands/mitsubishi.png" alt="Mitsubishi Logo" class=" max-h-12 w-full object-contain"/>
-                        <img width="158" height="48" src="/brands/suzuki.png" alt="Suzuki Logo" class=" max-h-12 w-full object-contain"/>
-                        <img width="158" height="48" src="/brands/toyota.png" alt="Toyota Logo" class=" max-h-12 w-full object-contain"/>
-                    </div>
+                <!-- BRANDS -->
+                <div class="rounded-b-lg shadow-base-300/20 shadow-lg mx-auto mt-10 grid max-w-xl items-center gap-x-8 gap-y-10 pb-8 grid-cols-2 md:grid-cols-5 md:mx-0 md:max-w-none">
+                    <img width="158" height="48" src="/brands/dominos-pizza.webp" alt="Dominos Pizza Logo" class="max-h-12 w-full object-contain"/>
+                    <img width="158" height="48" src="/brands/mcdo.webp" alt="Mcdo Logo" class=" max-h-12 w-full object-contain"/>
+                    <img width="158" height="48" src="/brands/san-miguel.webp" alt="San Miguel Corporation Logo" class="max-h-12 w-full object-contain"/>
+                    <img width="158" height="48" src="/brands/anchor-land.png" alt="Anchor Land Holdings Logo" class=" max-h-12 w-full object-cover"/>
+                    <img width="158" height="48" src="/brands/megaworld.png" alt="Megaworld Logo" class=" max-h-12 w-full object-contain"/>
                     
-                    <div class="flex justify-center mt-8">
-                        <a href="#contact-us" class="rounded-full px-4 py-1.5 text-xs ring-1 ring-abyan-blue-200 bg-abyan-blue-50">
-                            ... And Many More. Interested Working Together?<span class="font-bold text-abyan-blue-500" > Reach Out</span>
-                        </a>
-                    </div>
+                    <img width="158" height="48" src="/brands/honda.png" alt="Honda Logo" class=" max-h-12 w-full object-contain" />
+                    <img width="158" height="48" src="/brands/hyundai.png" alt="Hyundai Logo" class=" max-h-12 w-full object-contain" />
+                    <img width="158" height="48" src="/brands/mitsubishi.png" alt="Mitsubishi Logo" class=" max-h-12 w-full object-contain"/>
+                    <img width="158" height="48" src="/brands/suzuki.png" alt="Suzuki Logo" class=" max-h-12 w-full object-contain"/>
+                    <img width="158" height="48" src="/brands/toyota.png" alt="Toyota Logo" class=" max-h-12 w-full object-contain"/>
+                </div>
+                
+                <div class="flex justify-center mt-8">
+                    <a href="#contact-us" class="rounded-full px-4 py-1.5 text-xs text-center ring-1 ring-abyan-blue-200 bg-abyan-blue-50">
+                        ... And Many More. Interested Working Together?<span class="font-bold text-abyan-blue-500" > Reach Out</span>
+                    </a>
                 </div>
             </section>
         </UContainer>
