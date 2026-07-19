d3e3d6157a73cb463b1059eb798f00f4ad4bad77
unbreykabol <106237244+unbreykabol@users.noreply.github.com>
2026-07-20 06:16:59 +0800

    rewrote code acording to nuxtui standard

--- app/components/sections/AboutUs.vue
+++ app/components/sections/AboutUs.vue
@@ -1,31 +1,21 @@
 <template>
-    <div id="about-us" class="w-full px-6 md:px-16 py-12 bg-abyan-white">
-        <UContainer>
-            <section>
-                <div>
-                    <div class="text-center">
-                        <h2>About Us</h2>
-                        <p>
-                            Founded in November 2015, Abyan Mgt. Services Corp. set out to bridge the relationship 
-                            between labor and capital. We're a corporation duly registered with the Securities and 
-                            Exchange Commission, and a legitimate service contractor under DOLE Department Order No. 174.
-                        </p>
-                        <p>
-                            As we grow, innovation in how we work will be continuous — but the dignity of work and the 
-                            rights of our workers will always come first.
-                        </p>
-                    </div>
-                    <UMarquee>
-                        <div>
-                            <img src="/images/man-digging-soil.jpg" class="w-30 rounded-3xl overflow-hidden">
-                            <img src="/images/man-operating-farm-equipment.jpg" class="w-30 rounded-3xl overflow-hidden">
-                            <img src="/images/man-spraying-plants.jpg" class="w-30 rounded-3xl overflow-hidden">
-                            <img src="/images/men-group-photo-v1.jpg" class="w-30 rounded-3xl overflow-hidden">
-                            <img src="/images/mwn-sorting-fruit.jpg" class="w-30 rounded-3xl overflow-hidden">
-                        </div>
-                    </UMarquee>
-                </div>
-            </section>
-        </UContainer>
-    </div>
+    <UPageSection
+        id="about-us"
+        title="About Us"
+        description="Founded in November 2015, Abyan Mgt. Services Corp. set out to bridge the relationship 
+        between labor and capital. We're a corporation duly registered with the Securities and Exchange Commission, 
+        and a legitimate service contractor under DOLE Department Order No. 174. As we grow, innovation in how we 
+        work will be continuous — but the dignity of work and the rights of our workers will always come first."
+        :ui="{
+            root: 'bg-abyan-white'
+        }"
+    >
+        <UMarquee class="mt-2" :ui="{ root: '[--gap:--spacing(4)]', content: 'w-auto py-1' }">
+            <img src="/images/man-digging-soil.jpg" class="w-50 aspect-5/7 object-cover rounded-3xl">
+            <img src="/images/man-operating-farm-equipment.jpg" class="w-50 aspect-5/7 object-cover rounded-3xl">
+            <img src="/images/man-spraying-plants.jpg" class="w-50 aspect-5/7 object-cover rounded-3xl">
+            <img src="/images/men-group-photo-v1.jpg" class="w-50 aspect-5/7 object-cover rounded-3xl">
+            <img src="/images/mwn-sorting-fruit.jpg" class="w-50 aspect-5/7 object-cover rounded-3xl">
+        </UMarquee>
+    </UPageSection>
 </template>
--- app/components/sections/ContactUs.vue
+++ app/components/sections/ContactUs.vue
@@ -1,40 +1,75 @@
 <template>
-    <div id="contact-us" class="w-full px-6 md:p-16 py-4 bg-abyan-blue-700">
-        <UContainer>
-            <section class="grid items-start gap-8 md:grid-cols-3">
+    <UPageSection
+        id="contact-us"
+        title="Get In Touch"
+        description="Bridging Relationships Between Labor and Capital. Reach out today to discuss a business partnership, or submit your resume to join our talent pool. You can contact our team directly or visit our main office."
+        orientation="horizontal"
+        :ui="{
+            root: 'bg-abyan-blue-500',
+            title: 'text-abyan-white',
+            description: 'text-abyan-white'
+        }"
+    >
+        <UPageGrid>
+            <UPageCard
+                title="Call us"
+                description="(02)8731-6384      (02)8255-5762"
+                :ui="{
+                    root: 'bg-abyan-blue-100',
+                    title: 'text-abyan-blue-500',
+                    description: 'text-xs font-bold text-abyan-blue-800'
+                }"
+            />
 
-                <div class="gap-4 text-abyan-white">
-                    <h2>Get In Toooouch</h2>
-                    <p class="text-sm">
-                        Bridging Relationships Between Labor and Capital. 
-                        Reach out today to discuss a business partnership, or submit your 
-                        resume to join our talent pool. You can contact our team directly 
-                        or visit our main office.
-                    </p>
-                </div>
+            <UPageCard
+                title="Send us your resume"
+                description="yourfuturestartshere2025@gmail.com"
+                :ui="{
+                    root: 'bg-abyan-blue-100',
+                    title: 'text-abyan-blue-500',
+                    description: 'text-xs font-bold text-abyan-blue-800'
+                }"
+            />
 
-                <div class="grid gap-4 md:grid-cols-2 md:col-span-2">                    
-                    <div class="rounded-2xl p-6 bg-abyan-blue-100">
-                        <p class="py-2 text-abyan-blue-500">Call us</p>
-                        <p class="text-xs font-bold text-abyan-blue-800">(02)8731 6384</p>
-                    </div>
-                
-                    <div class="rounded-2xl p-6 bg-abyan-blue-100">
-                        <p class="py-2 text-abyan-blue-500">Send us your resume</p>
-                        <p class="text-xs font-bold text-abyan-blue-800">yourfuturestartshere2025@gmail.com</p>
-                    </div>
+            <UPageCard
+                title="Visit our main office"
+                description="3rd Floor, Unit 18, Legazpi Tower, 2600 Roxas Blvd, Malate, Manila, 1004 Metro Manila."
+                :ui="{
+                    root: 'bg-abyan-blue-100',
+                    title: 'text-abyan-blue-500',
+                    description: 'text-xs font-bold text-abyan-blue-800'
+                }"
+            />
 
-                    <div class="rounded-2xl p-6 bg-abyan-blue-100"> 
-                        <p class="py-2 text-abyan-blue-500">Visit us</p>
-                        <p class="text-xs font-bold text-abyan-blue-800">3rd Floor, Unit 18, Legazpi Tower, 2600 Roxas Blvd, Malate, Manila, 1004 Metro Manila</p>
-                        <p class="mt-4 text-xs font-bold text-shadow-abyan-blue-800">Our offices are open during business hours</p>
-                    </div>
+            <UPageCard
+                title="Iloilo office"
+                description="#51 Valeria Extension Street, Brgy. Nonoy Iloilo City."
+                :ui="{
+                    root: 'bg-abyan-blue-100',
+                    title: 'text-abyan-blue-500',
+                    description: 'text-xs font-bold text-abyan-blue-800'
+                }"
+            />
 
-                    <div class="hidden rounded-2xl p-6 bg-abyan-white">
-                    </div>
-                </div>
-
-            </section>
-        </UContainer>
-    </div>
+            <UPageCard
+                title="Tacloban office"
+                description="Door 5 Lot 2 Block 1 Lolita Homes Subdivision, Brgy. Guindapunan, Palo, Leyte."
+                :ui="{
+                    root: 'bg-abyan-blue-100',
+                    title: 'text-abyan-blue-500',
+                    description: 'text-xs font-bold text-abyan-blue-800'
+                }"
+            />
+            
+            <UPageCard
+                title="Davao office"
+                description="Door 6, Plaza De Tavera Bldg. Camus Ext., Davao City."
+                :ui="{
+                    root: 'bg-abyan-blue-100',
+                    title: 'text-abyan-blue-500',
+                    description: 'text-xs font-bold text-abyan-blue-800'
+                }"
+            />
+        </UPageGrid>
+    </UPageSection>
 </template>
