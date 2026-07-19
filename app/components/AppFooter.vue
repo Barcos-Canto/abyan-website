6ead3d9bf31bba9cbb0d102e7aa48ed510dc97d6
unbreykabol <106237244+unbreykabol@users.noreply.github.com>
2026-07-19 17:11:39 +0800

    fixed class typo

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -5,7 +5,7 @@
 			<div class="flex h-6 w-auto gap-2">
             	<img
 					src="/logos/abyan-logo-white-cropped.png"
-					class="w-auto h-auto bject-contain"
+					class="w-auto h-auto object-contain"
 					alt="Abyan Logo"
 			  	></img>
 				<span class=" text-abyan-white text-xl font-bold">ABYAN</span>
