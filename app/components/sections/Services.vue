f294717bbe2dc4a3f87f51ccadb6d4631249048f
cesar <cesarcantopaul@gmail.com>
2026-07-20 14:15:38 +0800

    added text contents and structure

--- app/components/sections/Services.vue
+++ app/components/sections/Services.vue
@@ -1,7 +1,55 @@
+<script setup lang="ts">
+import type { PageFeatureProps, ButtonProps } from '@nuxt/ui'
+
+const features = ref<PageFeatureProps[]>([
+  {
+    title: 'Management Consultancy',
+    description: 'Nuxt UI integrates with Nuxt Icon to access over 200,000+ icons from Iconify.',
+    icon: 'i-lucide-chess-rook',
+    ui: {
+            title: 'text-abyan-white',
+            description: 'text-abyan-blue-200',
+            leadingIcon: 'text-abyan-white'     
+    }
+  },
+  {
+    title: 'Manpower',
+    description: 'Nuxt UI integrates with Nuxt Fonts to provide plug-and-play font optimization.',
+    icon: 'i-lucide-users',
+    ui: {
+        title: 'text-abyan-white',
+        description: 'text-abyan-blue-200',
+        leadingIcon: 'text-abyan-white'     
+    }
+  },
+  {
+    title: 'Human Capital Development',
+    description: 'Nuxt UI integrates with Nuxt Color Mode to switch between light and dark.',
+    icon: 'i-lucide-chart-spline',
+    ui: {
+            title: 'text-abyan-white',
+            description: 'text-abyan-blue-200',
+            leadingIcon: 'text-abyan-white'     
+    }
+  }
+])
+</script>
+
 <template>
-    <div>
-        <section>
-            <h2>Services</h2>
-        </section>
-    </div>
+    <UPageSection
+        id="services"
+        class="bg-abyan-blue-500"
+        title="Comprehensive Workplace Solutions"
+        headline="What We Deliver"
+        description="Our services are rooted in fostering strong relationships, promoting career growth, and ensuring that both job seekers and businesses thrive in a collaborative and conflict-free environment."
+        orientation="horizontal"
+        reverse
+        :features="features"
+        :ui="{
+            title: 'text-abyan-white',
+            description: 'text-abyan-blue-200'
+        }"
+    >
+
+    </UPageSection>
 </template>
