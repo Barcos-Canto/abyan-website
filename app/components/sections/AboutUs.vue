ab4899e5442c19bea88e9af7a568bea9023f7426
cesar <cesarcantopaul@gmail.com>
2026-07-20 14:38:02 +0800

    about us section --first draft

--- app/components/sections/AboutUs.vue
+++ app/components/sections/AboutUs.vue
@@ -1,11 +1,48 @@
+<script setup lang="ts">
+import type { PageFeatureProps, ButtonProps } from '@nuxt/ui'
+
+const features = ref<PageFeatureProps[]>([
+  {
+    title: 'Integrity',
+    icon: 'i-lucide-scale',
+  },
+  {
+    title: 'Loyalty',
+    icon: 'i-lucide-handshake',
+  },
+  {
+    title: 'Professionalism',
+    icon: 'i-lucide-briefcase-business',
+  },
+  {
+    title: 'Teamwork',
+    icon: 'i-lucide-puzzle',
+  },
+  {
+    title: 'Concern for Others',
+    icon: 'i-lucide-hand-heart',
+  },
+  {
+    title: 'Disciplined and Responsible',
+    icon: 'i-lucide-anchor',
+    ui: {
+            title: 'text-abyan-blue-950',
+            leadingIcon: 'text-abyan-blue-500'     
+    }
+  },
+])
+</script>
+
 <template>
     <UPageSection
         id="about-us"
         title="About Us"
         description="Founded in November 2015, Abyan Mgt. Services Corp. set out to bridge the relationship 
-        between labor and capital. We're a corporation duly registered with the Securities and Exchange Commission, 
-        and a legitimate service contractor under DOLE Department Order No. 174. As we grow, innovation in how we 
-        work will be continuous — but the dignity of work and the rights of our workers will always come first."
+        between labor and capital. As we grow, innovation in how we work will be continuous — but the dignity 
+        of work and the rights of our workers will always come first. We continue to pursue in imparting our 
+        core values among our talents:"
+        orientation="horizontal"
+        :features="features"
         :ui="{
             root: 'bg-abyan-white'
         }"
