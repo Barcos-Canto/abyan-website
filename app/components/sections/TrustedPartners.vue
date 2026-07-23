1c2155cccc9b9221874fa8fb1b40206bc89c733e
cesar <cesarcantopaul@gmail.com>
2026-07-23 14:31:20 +0800

    changed alignment, cta, text content

--- app/components/sections/TrustedPartners.vue
+++ app/components/sections/TrustedPartners.vue
@@ -1,11 +1,13 @@
 <template>
     <UPageSection
         id="partners"
-        headline="Fostering partnerships since 2015"
+        headline="Trusted since 2015 — and still growing with the partners who believed in us first"
         title="Trusted By Known Brands"
         :ui="{
             root: 'bg-abyan-white',
-            container: 'py-12 sm:py-12 sm:gap-8 lg:py-12',
+            container: 'py-12 sm:py-12 gap-4 sm:gap-4 lg:py-12',
+            title: 'flex justify-start mb-4 text-abyan-blue-950',
+            headline: 'flex justify-start text-abyan-blue-900',
         }"
     >
         <div class="rounded-b-lg shadow-abyan-blue-900/10 shadow-lg mx-auto grid max-w-xl items-center gap-x-8 gap-y-8 pb-4 grid-cols-2 md:grid-cols-5 md:mx-0 md:max-w-none">
@@ -24,11 +26,10 @@
         </div>
 
         <div class="flex justify-center animate-bounce">
-            <a href="#contact-us" class="rounded-full px-4 py-1.5 text-xs text-center ring-1 ring-abyan-blue-200 bg-abyan-blue-50">
+            <a href="#contact-us" class="rounded-full px-4 py-2 mt-8 text-xs text-center ring-1 ring-abyan-blue-200 bg-abyan-blue-50">
                 ... And Many More. Interested Working Together?
                 <span class="font-bold text-abyan-blue-500" > 
                     Reach Out
-                    <UIcon name="i-lucide-chevrons-down"/>
                 </span>
             </a>
         </div>
