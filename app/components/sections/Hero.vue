8e63d27f9fefa41619539e5ea06642072930e3e0
cesar <cesarcantopaul@gmail.com>
2026-07-23 10:52:18 +0800

    added headline

--- app/components/sections/Hero.vue
+++ app/components/sections/Hero.vue
@@ -2,13 +2,15 @@
     <UPageHero
         id="hero"
         title="Building Relationships Between Labor And Capital"
-        description="Uniting workforce and management for mutual growth and success. From market beginnings to industry impact. Abyan bridges gaps between workforce and management, building strong foundations for career growth and mutual success."
+        description="From market beginnings to industry impact — Abyan bridges gaps between workforce and management, building strong foundations for career growth and mutual success."
+        headline="Uniting workforce and management for mutual growth and success"
         orientation="horizontal"
         :ui="{
             root: 'bg-abyan-blue-950',
             container: 'py-12 sm:py-12 lg:py-12',
             title: 'text-abyan-white',
-            description: 'text-abyan-blue-200'
+            description: 'text-abyan-blue-200',
+            headline: 'text-abyan-blue-200'
         }"
     >
         <img
