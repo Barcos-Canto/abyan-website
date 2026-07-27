f43d5b235ee413048edcec57af3fd986aa9b35b5
cesar <cesarcantopaul@gmail.com>
2026-07-27 10:50:37 +0800

    added new page section

---
+++ app/components/sections/FAQs.vue
@@ -0,0 +1,5 @@
+<tempate>
+    <UPageSection
+    
+    />
+</tempate>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,4 +5,5 @@
     <SectionsAboutUs />
     <SectionsCompanyValues />
     <SectionsContactUs />
+    <SectionsFAQs />
 </template>
