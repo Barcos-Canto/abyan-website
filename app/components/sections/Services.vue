d6eaa710653a778e92ce273fef43b52ab306832b
cesar <cesarcantopaul@gmail.com>
2026-07-27 10:22:02 +0800

    made images responsive

--- app/components/sections/Services.vue
+++ app/components/sections/Services.vue
@@ -38,7 +38,7 @@ const features = ref<PageFeatureProps[]>([
         class="bg-abyan-blue-950"
         title="Comprehensive Workplace Solutions"
         headline="What We Deliver"
-        description="Our services are rooted in fostering strong relationships, promoting career growth, and ensuring that both job seekers and businesses thrive in a collaborative and conflict-free environment."
+        description="Our ssservices are rooted in fostering strong relationships, promoting career growth, and ensuring that both job seekers and businesses thrive in a collaborative and conflict-free environment."
         orientation="horizontal"
         reverse
         :features="features"
@@ -49,15 +49,25 @@ const features = ref<PageFeatureProps[]>([
             container: 'py-12 sm:py-12 lg:py-12',
         }"
     >
-		<UPageGrid class="gap-2">
+		<UPageGrid class="grid-cols-2 gap-2">
 			<UPageCard 
-				class="bg-abyan-blue-950 lg:col-span-2"
+				class="bg-abyan-blue-950 col-span-2" 
 				:ui="{
 					root: 'ring-0',
 					container: 'p-0 sm:p-0'
 				}"
 			>
-				<img src="/images/group-photo-men-v2.webp" class="w-full h-64 rounded-xl object-cover">
+				<img src="/images/group-photo-men-v2.webp" class="w-full aspect-video rounded-xl object-cover">
+			</UPageCard>
+
+			<UPageCard 
+				class="bg-abyan-blue-950 lg:self-end"
+				:ui="{
+					root: 'ring-0',
+					container: 'p-0 sm:p-0'
+				}"
+			>
+				<img src="/images/man-operating-farm-equipment.webp" class="w-full aspect-square rounded-xl object-cover">
 			</UPageCard>
 
 			<UPageCard 
@@ -67,27 +77,17 @@ const features = ref<PageFeatureProps[]>([
 					container: 'p-0 sm:p-0'
 				}"
 			>
-				<img src="/images/man-operating-farm-equipment.webp" class="w-full h-64 rounded-xl object-cover">
+				<img src="/images/group-photo-women-v1.webp" class="w-full aspect-square rounded-xl object-cover">
 			</UPageCard>
 
 			<UPageCard 
-				class="bg-abyan-blue-950"
+				class="bg-abyan-blue-950 col-span-2"
 				:ui="{
 					root: 'ring-0',
 					container: 'p-0 sm:p-0'
 				}"
 			>
-				<img src="/images/group-photo-women-v1.webp" class="w-full h-64 rounded-xl object-cover">
-			</UPageCard>
-
-			<UPageCard 
-				class="bg-abyan-blue-950 lg:col-span-2"
-				:ui="{
-					root: 'ring-0',
-					container: 'p-0 sm:p-0'
-				}"
-			>
-				<img src="/images/group-photo-men-v1.webp" class="w-full h-64 rounded-xl object-cover">
+				<img src="/images/group-photo-men-v1.webp" class="w-full aspect-video rounded-xl object-cover">
 			</UPageCard>
 		</UPageGrid>
     </UPageSection>
