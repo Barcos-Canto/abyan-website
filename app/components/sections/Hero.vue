a46046ecf577ed7b9908af4ca66fc697fb2baf6f
cesar <cesarcantopaul@gmail.com>
2026-07-29 14:47:10 +0800

    content: remove headline

--- app/components/sections/Hero.vue
+++ app/components/sections/Hero.vue
@@ -3,14 +3,12 @@
         id="hero"
         title="Building Relationships Between Labor And Capital"
         description="From market beginnings to industry impact — Abyan bridges gaps between workforce and management, building strong foundations for career growth and mutual success."
-        headline="Uniting workforce and management for mutual growth and success"
         orientation="horizontal"
         :ui="{
             root: 'bg-abyan-blue-950',
             container: 'py-12 sm:py-12 lg:py-12 gap-12 sm:gap-y-12',
             title: 'text-abyan-white',
             description: 'text-abyan-blue-200',
-            headline: 'text-abyan-blue-200'
         }"
     >
         <img
