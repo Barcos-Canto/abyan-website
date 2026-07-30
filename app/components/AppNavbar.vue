ff3a2ab9aed20a420c0b4051165c9e43b05e33cf
cesar <cesarcantopaul@gmail.com>
2026-07-30 14:05:48 +0800

    feat: add new button for careers page

--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -55,6 +55,14 @@ const items = computed<NavigationMenuItem[]>(() => [
 
         <template #right>
             <UButton
+                label="Apply"
+				icon="i-lucide-package-search"
+				size="lg"
+                class="rounded-full border font-bold bg-abyan-blue-950 border-abyan-blue-50 text-abyan-blue-50 hover:bg-abyan-blue-50 hover:text-abyan-blue-950"
+                to=""
+                target="_blank"
+            />
+            <UButton
                 label="Tracking"
 				icon="i-lucide-package-search"
 				size="lg"
