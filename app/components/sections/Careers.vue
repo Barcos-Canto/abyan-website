2ef9db0062bf0e795b50e6a6052593bb0cae06c3
cesar <cesarcantopaul@gmail.com>
2026-08-03 10:54:09 +0800

    feat: add file upload and form placeholder

--- app/components/sections/Careers.vue
+++ app/components/sections/Careers.vue
@@ -1,5 +1,20 @@
 <template>
     <UPageSection
         title="Careers"
-    />
+        orientation="horizontal"
+    >
+        <template #description>
+            Name:__________
+        </template>
+
+        <UFileUpload
+            :dropzonze="true"
+            :interactive="true"
+            highlight
+            accept="application/pdf"
+            label="Upload Your Resume"
+            description="PDF (max 2MB)"
+            size="xl"
+        />
+    </UPageSection>
 </template>
