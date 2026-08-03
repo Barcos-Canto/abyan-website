0a98d67f4f2b388c984e8cc63e64022832f3184e
cesar <cesarcantopaul@gmail.com>
2026-08-03 10:54:54 +0800

    chore: add img and supabase pkg/modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2025-07-15',
-	devtools: { enabled: true },
-	modules: ['@nuxt/ui'],
-	css: ['~/assets/css/main.css'],
-	ui: {colorMode: false}
+				compatibilityDate: '2025-07-15',
+				devtools: { enabled: true },
+				modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/supabase'],
+				css: ['~/assets/css/main.css'],
+				ui: {colorMode: false}
 })
